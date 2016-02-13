c2029585363ca1e7fbae433e3084a11f23753305
Oscar <devos>
2016-02-13 19:05:43 +0100

    Add project for hosting service

---
+++ WcfServiveTest01/ServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+
+namespace ServiceHost
+{
+    /// <summary>
+    /// Class to host Service
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.ServiceModel.ServiceHost host = new System.ServiceModel.ServiceHost(typeof(ProductsService));
+            host.Open();
+            Console.WriteLine("Service STARTED...");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
--- WcfServiveTest01/WcfServiceLibrary1/ProductsService.cs
+++ WcfServiveTest01/WcfServiceLibrary1/ProductsService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace WcfServiceLibrary1
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class ProductsService : IProductsService
     {
         public string GetData(int value)
         {
