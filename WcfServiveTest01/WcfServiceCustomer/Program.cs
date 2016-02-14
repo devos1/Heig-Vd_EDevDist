39f1608e0ded25815578ab932ad0ab20c3691b42
Oscar <devos>
2016-02-14 16:54:31 +0100

    Add project "ServiceClient" and call remote function ok

---
+++ WcfServiveTest01/WcfServiceCustomer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceCustomer.ServiceRefProducts;
+
+namespace WcfServiceCustomer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductsServiceClient proxy = new ProductsServiceClient();
+            Console.WriteLine("Appel de la méthode distante...");
+            Console.WriteLine(proxy.GetData(99));
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
